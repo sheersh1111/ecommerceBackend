ea35e5c579c365ab287b35e8133ebcd4a4be66b5
sheersh1111 <106367218+sheersh1111@users.noreply.github.com>
2023-04-23 01:53:49 +0530

    Update paymentControllers.js

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -1,4 +1,4 @@
-const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors.js");
 
 const stripe=require("stripe")(process.env.STRIPE_SECRET_KEY);
 
